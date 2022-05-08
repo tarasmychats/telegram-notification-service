5871a5527c2ae1191614464643cc03fac9e72940
Taras Mykhats <>
2022-05-08 21:23:46 +0300

    added https redirection

--- src/TelegramNotificationService/Program.cs
+++ src/TelegramNotificationService/Program.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+    options.HttpsPort = builder.Environment.IsProduction() ? 443 : 7234;
+});
 
 var app = builder.Build();
 
